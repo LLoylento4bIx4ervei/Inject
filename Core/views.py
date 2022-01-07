9a1d6b25f5ea45fd98a676cc6fd786a6a8532947
LLoylento4bIx4ervei <68648015+LLoylento4bIx4ervei@users.noreply.github.com>
2022-01-07 14:12:31 +0300

    We add in progect users app. In settings apps add users. Create two html file in templates. Create user form. In models create class profile

--- Core/views.py
+++ Core/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
 
 
-def home (request):
-    return HttpResponse('Hello World')
+def home(request):
+
+    return render(request, 'Core/HomePage.html')
--- Inject/settings.py
+++ Inject/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Users.apps.UsersConfig',
+    'Core.apps.CoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
